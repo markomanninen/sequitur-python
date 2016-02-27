5bae4e20d6e5948d10bb74e763cd0f1f1ec68f78
Marko Manninen <mmstud@gmail.com>
2016-02-27 13:24:19 +0200

    changed unique rule index
    rule index was a rule class based constant thus multiple grammar
    instances would never reset the index. now rule indeed are grammar
    specific. every new instance of grammar will start rules unique index
    from 0

--- rule.py
+++ rule.py
@@ -1,18 +1,16 @@
-from symbol import Symbol
-
+from .symbol import Symbol
 
 class Rule(object):
-    
-    unique_rule_number = 1
-    
+    """docstring for Rule"""
+
     def __init__(self, grammar):
         super(Rule, self).__init__()
         self.guard = Symbol.guard(grammar, self)
         self.guard.join(self.guard)
         self.reference_count = 0
-        self.unique_number = Rule.unique_rule_number
-        Rule.unique_rule_number += 1
-    
+        self.unique_number = grammar.unique_rule_number
+        grammar.unique_rule_number += 1
+
     def first(self): return self.guard.next
 
     def last(self): return self.guard.prev
@@ -20,6 +18,15 @@ class Rule(object):
     def increment_reference_count(self): self.reference_count += 1
 
     def decrement_reference_count(self): self.reference_count -= 1
+
+    def get_rule(self, rule_set):
+        """docstring for get_rule"""
+        symbol = self.first()
+        output_array = []
+        while not symbol.is_guard():
+            output_array.append(symbol.get_rule(rule_set))
+            symbol = symbol.next
+        return output_array
 
     def print_rule(self, rule_set, output_array, line_length):
         """docstring for print_rule"""
@@ -36,4 +43,3 @@ class Rule(object):
             line_length = symbol.print_rule_expansion(rule_set, output_array, line_length)
             symbol = symbol.next
         return line_length
-        
